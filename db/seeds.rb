6faaca65c5f493c1ac23bff6fd7ae7c11a79ae3e
pedro <pedro.silva@pontomais.com.br>
2021-07-04 00:25:07 -0300

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,42 @@
+
+Airline.create([
+  { 
+    name: "United Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+  }, 
+  { 
+    name: "Southwest",
+    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+  },
+  { 
+    name: "Delta",
+    image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+  }, 
+  { 
+    name: "Alaska Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+  }, 
+  { 
+    name: "JetBlue",
+    image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+  }, 
+  { 
+    name: "American Airlines",
+    image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+  }
+])
+
+Review.create([
+  {
+    title: "Great airline",
+    description: "I had a lovely time.",
+    score: 5,
+    airline: Airline.first
+  },
+  {
+    title: "Bad airline",
+    description: "I had a fuckly time.",
+    score: 0,
+    airline: Airline.second
+  }
+])
